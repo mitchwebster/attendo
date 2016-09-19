4b53233847a7b1b51fcc44b2be451962a198a9ce
Mitch Webster <mitchell.a.webster@gmail.com>
2016-09-19 16:46:14 -0400

    Added mongodb client to the mix

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
-
 var MongoClient = require('mongodb').MongoClient;
+var dbConfig = require('./dbConfig');
 
 var portNumber = 8080;
 var app = express();
 
-app.post('/api/test', function(req, res) {
-	// console.log(req);
-	console.log("Someone hit the test api");
-	res.send({err : false, msg: "Thank you for hitting our api"});
-});
-
 console.log('Listening on port ', portNumber);
 
-app.listen(portNumber);
+MongoClient.connect(dbConfig.url, function(err, db) {
+	if (err) {
+		console.log("Could not connect to the database");
+		console.log(err);
+	} else {
+		var collection = db.collection('Users');
+
+		var testDoc = {"username": "mitch"};
+
+		//routes
+		app.post('/api/test', function(req, res) {
+			// console.log(req);
+			console.log("Someone hit the test api");
+			collection.insert(testDoc, {w:1}, function(err, result) {
+				if (err) {
+					res.send({err : true, msg: "Something went wrong"});
+				} else {
+					res.send({err : false, msg: "Thank you for hitting our api"});
+				}
+			});
+		});
+		
+		app.listen(portNumber);
+	}
+});
