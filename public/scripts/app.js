ebc7cf02a4a8462661cd36c1f159135be86e48ba
Mitch Webster <mitchell.a.webster@gmail.com>
2016-11-04 15:55:18 -0400

    finished user setup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,7 +13,10 @@
         .state('courses', {
             url: "/courses",
             controller: 'coursesCtrl',
-            templateUrl: "views/coursesview.html"
+            templateUrl: "views/coursesview.html",
+            params: {
+                user: null
+            }
         })
         .state('course', {
             url: "/course",
@@ -28,19 +31,19 @@
     .controller('loginCtrl', function($scope, $http, $mdDialog, $state) {
             $scope.submitLogin = function() {
                 //need to submit the user to CAS
-                alert(JSON.stringify($scope.user));
-                //need to switch views too
-                $state.go('courses');
+                console.log($scope.user);
+                $state.go('courses', {user: $scope.user});
             };
         })
-    .controller('coursesCtrl', function($scope, $http, $mdDialog, $state) {
+    .controller('coursesCtrl', function($scope, $http, $mdDialog, $state, $stateParams) {
             // $scope.submitLogin = function() {
                 //need to submit the user to CAS
                 // alert(JSON.stringify($scope.user));
                 //need to switch views too
                 // $state.go('courses');
-                var temp = {username: "mwebster7"};
-                $http.post('/api/myCourses', temp).then(function successCallback(response) {
+                $scope.user = $stateParams.user;
+                $scope.selected = {};
+                $http.post('/api/myCourses', {username: $scope.user.username}).then(function successCallback(response) {
                     response = response.data;
                     if (response.err) {
                         console.log(response);
@@ -50,16 +53,79 @@
                             $scope.courses = response.courses;
                         } else {
                             $scope.userExists = false;
+                            //TODO pull the tabs from tsquare
+                            var tempLabels = {courses : ["CS-3312-JIA,JID,LMC-", "CS-3510-A", "CS-4261-A", "CS-4641-A", "ECON-3300-EJ"]};
+                            $http.post('/api/coursePrompt', tempLabels).then(function successCallback(response) {
+                                response = response.data;
+                                if (response.err) {
+                                    console.log(response);
+                                } else {
+                                    $scope.coursePromptCourses = createCourseObjects(response.courses);
+                                    console.log($scope.coursePromptCourses);
+                                }
+                                console.log(response);
+                            }, function failedCallback(response) {
+                                console.log(response);
+                            });
                         }
                     }
                 }), function failedCallback(response) {
                     console.log(response);
                 };
 
+                function createCourseObjects(arr) {
+                    var output = {}
+                    for (var i = 0; i < arr.length; i++) {
+                        if (arr[i].courseName in output) {
+                            output[arr[i].courseName].push({section: arr[i].section, crn: arr[i].crn});
+                        } else {
+                            output[arr[i].courseName] = [{section: arr[i].section, crn: arr[i].crn}];
+                        }
+                    }
+                    return output;
+                }
+
                 $scope.selectClass = function(course) {
                     console.log(course);
                     $state.go('course', {course: course});
                 }
+
+                $scope.submitUser = function() {
+                    var courses = Object.keys($scope.selected);
+                    if (courses.length == Object.keys($scope.coursePromptCourses).length) {
+                        var output = []
+                        for (var i = 0; i < courses.length; i++) {
+                            output.push($scope.selected[courses[i]]);
+                        }
+                        output = {username: $scope.user.username, courses: output};
+                        console.log(output);
+                        $http.post('/api/userSetup', output).then(function successCallback(response) {
+                            response = response.data;
+                            if (response.err) {
+                                console.log(response);
+                            } else {
+                                $scope.userExists = true;
+                                $http.post('/api/myCourses', {username: $scope.user.username}).then(function successCallback(response) {
+                                    response = response.data;
+                                    if (response.err) {
+                                        console.log(response);
+                                    } else {
+                                        console.log(response);
+                                        if (response.userExists) {
+                                            $scope.courses = response.courses;
+                                        } else {
+                                            console.log("Some error occured");
+                                        }
+                                    }
+                                }), function failedCallback(response) {
+                                    console.log(response);
+                                };
+                            }
+                        }, function failureCallback(err) {
+                            console.log(err);
+                        });
+                }
+            }
             // };
         })
     .controller('singleCourseCtrl', function($scope, $http, $mdDialog, $state, $stateParams) {
